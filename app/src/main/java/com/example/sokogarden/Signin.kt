c6577a188569577c6f462df19b230fc2ea604e1d
Brayemar <mogikoyob@gmail.com>
2026-04-13 14:05:52 +0300

    added soko code

--- app/src/main/java/com/example/sokogarden/Signin.kt
+++ app/src/main/java/com/example/sokogarden/Signin.kt
@@ -1,10 +1,15 @@
 package com.example.sokogarden
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.loopj.android.http.RequestParams
 
 class Signin : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +21,38 @@ class Signin : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+//        find the two editText, a button and TextView by use of their ids.
+        val email = findViewById<EditText>(R.id.email)
+        val password = findViewById<EditText>(R.id.password)
+        val signinButton = findViewById<Button>(R.id.signinBtn)
+        val signupTextView = findViewById<TextView>(R.id.signuptxt)
+
+//        On the textView set on click listener such that when clicked it navigates you to the sign up Page
+        signupTextView.setOnClickListener {
+            val intent = Intent(applicationContext, Signup::class.java)
+            startActivity(intent)}
+
+//      On click of the button signin, we need to interact with our API endpoint as we pass the two data info email and password
+            signinButton.setOnClickListener {
+
+//                Specify the API endpoint
+                val api = "https://kbenkamotho.alwaysdata.net/api/signin"
+
+//              Create a request that will enable you to hold the data in form of a bundle/package
+                val data = RequestParams()
+
+//              Add/append/attach the email and the password
+                data.put("email", email.text.toString())
+                data.put("password", password.text.toString())
+
+//              Import the API helper
+                val helper = ApiHelper(applicationContext)
+
+//              By use of the function post_login inside of the helper class, post your data
+                helper.post_login(api, data)
+
+            }
+
+
     }
 }
